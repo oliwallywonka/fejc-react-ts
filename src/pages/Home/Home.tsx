848460d18801bad19790efd50b63548d677a346b
Alex Chura <yosoloyo56@gmail.com>
2022-11-24 22:57:04 -0400

    feat: Add Home Acordeon and grid Component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useArtworkByArtist } from '../../api/useArtworkByArtist';
 import { useCategoryByIds } from '../../api/useCategoriesByIds';
 import Accordion from '../../components/Accordion';
+import { GridItems } from '../../components/GridItems/GridItems';
 import IconLink from '../../components/IconLink';
 import { IconPainting } from '../../components/IconPainting/IconPainting';
 import { ArtworkByArtist } from '../../components/interfaces/ResponseArtWorks';
@@ -36,29 +37,20 @@ export const Home: FC = () => {
           <h2 className="title-2">
             Mr. Van G
           </h2>
-          <div></div>
-          {categoriesQuery.data.map(category => (
+          {categoriesQuery.data.map(category => {
             
-            <Accordion 
-              key={category.id} 
-              title={category.title} 
-              icon={
-                <IconPainting 
-                  imageId={artworkFilteredByCategory(category.id)[0].image_id}/>
-              }
-              expanded={true}>
-                <div className='grid gap-2 grid-cols-2'>
-                  {artworkFilteredByCategory(category.id).map(art => {
-                    return <IconLink 
-                      key={art.id} 
-                      label={art.title.length > 15? `${art.title.slice(0,15)}...`: art.title} 
-                      to={`/paintings/${art.id}`} 
-                      icon={art.image_id}/>
-                  })}
-                </div>
-            </Accordion>
-          ))}
-          
+            return (
+              <Accordion 
+                key={category.id} 
+                title={category.title} 
+                icon={
+                  <IconPainting 
+                    imageId={artworkFilteredByCategory(category.id)[0].image_id}/>
+                }>
+                  <GridItems artworks={artworkFilteredByCategory(category.id)} />
+              </Accordion>
+            )
+          })}
         </div>
       )}
     </>
